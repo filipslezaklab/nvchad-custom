757c0a742541781744d9cd0d24800dc6605ca9bd
Filip <filipslezaklab@gmail.com>
2023-05-09 14:27:36 +0200

    elo

--- mappings.lua
+++ mappings.lua
@@ -11,28 +11,56 @@ M.dap = {
   n = {
     ["<leader>db"] = {
       "<cmd> DapToggleBreakpoint <CR>",
-      "Toggle breakpoint"
+      "Toggle breakpoint",
     },
     ["<leader>dus"] = {
-      function ()
-        local windgets = require('dap.ui.widgets')
-        local sidebar = widgets.sidebar(widgets.scopes);
-        sidebar.open();
+      function()
+        local widgets = require "dap.ui.widgets"
+        local sidebar = widgets.sidebar(widgets.scopes)
+        sidebar.open()
       end,
-      "Open debugging sidebar"
-    }
-  }
+      "Open debugging sidebar",
+    },
+  },
 }
 
 M.crates = {
   n = {
     ["<leader>rcu"] = {
-      function ()
-        require('crates').upgrade_all_crates()
+      function()
+        require("crates").upgrade_all_crates()
       end,
-      "Update crates"
-    }
-  }
+      "Update crates",
+    },
+  },
+}
+
+M.spectre = {
+  n = {
+    ["<leader>S"] = {
+      function()
+        require("spectre").open()
+      end,
+      "Open Spectre",
+    },
+    ["<leader>sp"] = {
+      function()
+        require("spectre").open_file_search { select_word = true }
+      end,
+      "Search on current file",
+    },
+  },
+}
+
+M.fzf = {
+  n = {
+    ["<leader>gp"] = {
+      function()
+        require("fzf-lua").grep_project()
+      end,
+      "Grep project lines",
+    },
+  },
 }
 
 return M
--- plugins.lua
+++ plugins.lua
@@ -1,4 +1,4 @@
-local overrides = require("custom.configs.overrides")
+local overrides = require "custom.configs.overrides"
 
 ---@type NvPluginSpec[]
 local plugins = {
@@ -25,7 +25,7 @@ local plugins = {
   -- override plugin configs
   {
     "williamboman/mason.nvim",
-    opts = overrides.mason
+    opts = overrides.mason,
   },
 
   {
@@ -42,33 +42,33 @@ local plugins = {
     "rust-lang/rust.vim",
     init = function()
       vim.g.rustfmt_autosave = 1
-    end
+    end,
   },
 
   {
     "simrat39/rust-tools.nvim",
-    ft = {"rust"},
+    ft = { "rust" },
     dependencies = "neovim/nvim-lspconfig",
     opts = function()
       return require "custom.configs.rust-tools"
     end,
-    config = function(_, opts) 
-      require('rust-tools').setup(opts)
+    config = function(_, opts)
+      require("rust-tools").setup(opts)
     end,
   },
 
   -- DEBUG cia nvim-DAP
 
   {
-    'mfussenegger/nvim-dap',
+    "mfussenegger/nvim-dap",
   },
 
   {
-    'saecki/crates.nvim',
-    ft = {"rust", "toml"},
+    "saecki/crates.nvim",
+    ft = { "rust", "toml" },
     dependencies = "hrsh7th/nvim-cmp",
     config = function(_, opts)
-      local crates = require('crates')
+      local crates = require "crates"
       crates.setup(opts)
       crates.show()
     end,
@@ -76,10 +76,10 @@ local plugins = {
 
   -- override nvim-cmp
   {
-    'hrsh7th/nvim-cmp',
+    "hrsh7th/nvim-cmp",
     opts = function()
       local M = require "plugins.configs.cmp"
-      table.insert(M.sources, {name = "crates"})
+      table.insert(M.sources, { name = "crates" })
       return M
     end,
   },
@@ -95,21 +95,29 @@ local plugins = {
 
   -- emmet
   {
-    'mattn/emmet-vim',
+    "mattn/emmet-vim",
   },
 
   -- highlights for github actions
   {
     "yasuhiroki/github-actions-yaml.vim",
-    ft = {"yml"},
-  }
+    ft = { "yml" },
+  },
+
+  -- spectre for better search and replace operations
+  {
+    "nvim-pack/nvim-spectre",
+  },
+
+  {
+    "ibhagwan/fzf-lua",
+  },
 
   -- To make a plugin not be loaded
   -- {
   --   "NvChad/nvim-colorizer.lua",
   --   enabled = false
   -- },
-
 }
 
 return plugins
