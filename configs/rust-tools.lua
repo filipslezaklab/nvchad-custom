c8849ed010b2acdb4af3157f67cd269a05c3f5b8
Filip <filipslezaklab@gmail.com>
2023-05-19 12:22:55 +0200

    enable procmacro for rust

--- configs/rust-tools.lua
+++ configs/rust-tools.lua
@@ -3,8 +3,11 @@ local capabilities = require("plugins.configs.lspconfig").capabilities
 local options = {
   server = {
     standalone = true,
-    on_attach =  chad_on_attach,
+    on_attach = chad_on_attach,
     capabilities,
+    procMacro = {
+      enable = true,
+    },
   },
 }
 return options
